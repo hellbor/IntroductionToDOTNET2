68d55672172536d7542db52adeb420917e348e55
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-10-01 14:30:06 +0300

    Geometry DONE

--- Geometry/Program.cs
+++ Geometry/Program.cs
@@ -3,6 +3,7 @@
 #define TRIANGLE_2
 #define TRIANGLE_3
 #define TRIANGLE_4
+#define ROMBUS
 #define CHESS
 using System;
 using System.Collections.Generic;
@@ -17,19 +18,19 @@ namespace Geometry
 		static void Main(string[] args)
 		{
 			Console.Write("Введите размер фигуры: ");
-			int size = Convert.ToInt32(Console.ReadLine());
+			int n = Convert.ToInt32(Console.ReadLine());
 
 #if SQUARE
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < n; i++)
 			{
-				for (int j = 0; j < size - 1; j++) Console.Write("* ");
+				for (int j = 0; j < n - 1; j++) Console.Write("* ");
 				Console.WriteLine("* ");
 			}
 			Console.WriteLine();
 #endif
 
 #if TRIANGLE_1
-			for (int i = 0; i < size; i++)
+			for (int i = 0; i < n; i++)
 			{
 				for (int j = 0; j <= i; j++) Console.Write("* ");
 				Console.WriteLine(" ");
@@ -37,7 +38,7 @@ namespace Geometry
 			Console.WriteLine();
 #endif
 #if TRIANGLE_2
-			for (int i = size; i >= 1; i--)
+			for (int i = n; i >= 1; i--)
 			{
 				for (int j = 0; j < i; j++) Console.Write("* ");
 				Console.WriteLine(" ");
@@ -45,27 +46,52 @@ namespace Geometry
 			Console.WriteLine();
 #endif
 #if TRIANGLE_3
-			for (int i = size; i >= 1; i--)
+			for (int i = n; i >= 1; i--)
 			{
-				for (int j = size; j > i; j--) Console.Write("  ");
+				for (int j = n; j > i; j--) Console.Write("  ");
 				for (int k = 0; k < i; k++) Console.Write("* ");
 				Console.WriteLine();
 			}
 			Console.WriteLine();
 #endif
 #if TRIANGLE_4
-			for (int i = 1; i <= size; i++)
+			for (int i = 1; i <= n; i++)
 			{
-				for (int j = size; j > i; j--) Console.Write("  ");
+				for (int j = n; j > i; j--) Console.Write("  ");
 				for (int k = 0; k < i; k++) Console.Write("* ");
 				Console.WriteLine();
 			}
 			Console.WriteLine();
 #endif
-#if CHESS
-			for (int i = 0; i < size; i++)
+#if ROMBUS
+			for (int i = 0; i < n; i++)
 			{
-				for (int j = 0; j < size; j++)
+				for (int j = i; j < n; j++)
+					Console.Write(" ");
+					Console.Write("/");
+
+				for (int j = 0; j < i; j++)
+					Console.Write("  ");
+					Console.Write("\\");
+					Console.WriteLine();
+			}
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j <= i; j++)
+					Console.Write(" ");
+					Console.Write("\\");
+
+				for (int j = i; j < n - 1; j++)
+					Console.Write("  ");
+					Console.Write("/");
+					Console.WriteLine();
+			}
+            Console.WriteLine();
+#endif
+#if CHESS
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < n; j++)
 				{
 					if ((i + j) % 2 == 0) Console.Write("+ ");
 					else Console.Write("- ");
@@ -73,6 +99,6 @@ namespace Geometry
 				Console.WriteLine();
 			} 
 #endif
-		}
+        }
 	}
 }
