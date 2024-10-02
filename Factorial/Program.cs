e071882e7208174912c5a9eae40c075d6f964a60
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-10-02 15:44:38 +0300

    Factorial DONE

---
+++ Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			int factorial = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				factorial *= i;
+			}
+			Console.WriteLine($"Факториал числа {n} = {factorial}");
+		}
+	}
+}
