ee8e0c2373261d9fab38bf29213393e135ca99eb
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-10-05 19:55:19 +0300

    Snake DONE

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,14 +8,46 @@ namespace Snake
 {
 	internal class Program
 	{
+
 		static void Main(string[] args)
 		{
-			char key;
+			Console.SetBufferSize(Console.WindowWidth, Console.BufferHeight);
+			Random rand=new Random();
+			int x = rand.Next(Console.WindowWidth);
+			int y = rand.Next(Console.WindowHeight);
+			Console.CursorVisible = false;
+			ConsoleKey key;
 			do
 			{
-				key = Console.ReadKey().KeyChar;
-				Console.WriteLine($"{(int)key}\t{key}");
-			}while (true);
+				Console.Clear();
+				Console.SetCursorPosition(0, 0);
+				Console.ResetColor();
+                Console.WriteLine($"X={x}\t Y={y}");
+                Console.SetCursorPosition(x, y);
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine((char)2);
+				key = Console.ReadKey().Key;
+				//Console.WriteLine($"{(int)key}\t{key}");
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+					case ConsoleKey.W:	y--;	break;
+					case ConsoleKey.DownArrow:
+					case ConsoleKey.S:	y++;	break;
+					case ConsoleKey.LeftArrow:
+					case ConsoleKey.A:	x--;	break;
+					case ConsoleKey.RightArrow:
+					case ConsoleKey.D:	x++;	break;
+				}
+				if (
+					x == 0 || y == 0 ||
+					x == Console.WindowWidth || y == Console.WindowHeight)
+					Console.Beep();
+				if (x == 0) x = 1;
+				if (x == Console.WindowWidth) x = Console.WindowWidth - 1;
+				if (y == 0) y = 1;
+				if (x == Console.WindowHeight) y = Console.WindowHeight - 1;
+			} while (key != ConsoleKey.Escape);
 		}
 	}
 }
